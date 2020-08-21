e81c5b61df0a9b81a9d4e34a055c1adf3b7bdd34
Dev Patel <56962517+dmp060901@users.noreply.github.com>
2020-08-21 23:23:32 +0530

    Chnged in presence of 'Jagir A Shastri'
    Mota manaso aviya hata.......

--- TableV1/urls.py
+++ TableV1/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('table',view_table,name = "table")
+    path('table',view_table.as_view(),name = "table")
 ]
--- static/index.js
+++ static/index.js
@@ -1,4 +1,17 @@
 $(function(){
+    var csrftoken = Cookies.get('csrftoken');
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    // Ensure jQuery AJAX calls set the CSRF header to prevent security errors
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
     $(".draggable").draggable({
         revert: true,
         cursor: "move",
@@ -10,17 +23,61 @@ $(function(){
             color = select.attr("color");
         }
     }).disableSelection();
+    // console.log("hii");
     $( ".droppable" ).droppable({
         drop: function( event, ui ) {
         let obj = $( this );
         obj.html(name)
         obj.css({"background-color":color})
         obj.attr({"name":name ,"color":color,"event_link":event_link});
-        let s = obj.serialize()
         // obj.click(function() {
         //   open(event_link);
         // });
-        console.log(s);
         }
     });
+    $('.save').click(function(){
+        var myRows = [];
+        var headersText = [];
+        var $headers = $("th");
+        var $rows = $("tbody tr").each(function(index) {
+          $cells = $(this).find("td");
+          myRows[index] = {};
+
+          $cells.each(function(cellIndex) {
+            // Set the header text
+            if(headersText[cellIndex] === undefined) {
+              headersText[cellIndex] = $($headers[cellIndex]).text();
+            }
+            // Update the row object with the header/cell combo
+            myRows[index][headersText[cellIndex]] = $(this).attr("color");
+            // myRows[index][headersText[comboellIndex]] = $(this).attr("link");
+          });    
+        })
+        let state = JSON.stringify(myRows,null," ");
+        console.log(state);
+        // super($item, container);
+        $.ajax({
+            type: "POST",
+            data: state,
+            url: ""
+        });
+        // alert(state);
+    });
 });
+// $(function() {
+//         var csrftoken = Cookies.get('csrftoken');
+
+//     function csrfSafeMethod(method) {
+//         // these HTTP methods do not require CSRF protection
+//         return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+//     }
+
+//     // Ensure jQuery AJAX calls set the CSRF header to prevent security errors
+//     $.ajaxSetup({
+//         beforeSend: function(xhr, settings) {
+//             if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+//                 xhr.setRequestHeader("X-CSRFToken", csrftoken);
+//             }
+//         }
+// });
+

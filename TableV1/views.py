7512f82cdf92789b845f1bfd8a03148691e49fdf
Dev Patel <56962517+dmp060901@users.noreply.github.com>
2020-08-24 10:45:26 +0530

    nothing much

--- TableV1/views.py
+++ TableV1/views.py
@@ -1,39 +1,26 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-from .models import event_class,timings
-import json
-import datetime
-from django.views import View
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
-# Create your views here.
+from django.http import JsonResponse, HttpResponse
+from django.views import View
+
+#################
+import json
+import datetime
+from .models import event_class,timings
+######################
 
 class view_table(View):
 	template_name = "table.html"
 	@method_decorator(ensure_csrf_cookie)
 	def get(self, request):
-		# dbms = event_class(event_name = 'DBMS',event_link = "",event_color = "red")
-		# ds = event_class(event_name = 'DS',event_link = "",event_color = "#0f0")
-		# aem = event_class(event_name = 'AEM',event_link = "",event_color = "salmon")
-		# math = event_class(event_name = 'Math',event_link = "",event_color = "cyan")
-		# dbms.save()
-		# ds.save()
-		# aem.save()
-		# math.save()
-		# s = datetime.time(1,30,0)
-		# e = datetime.time(2,30,0)
-		# # lect1 = timings.objects.create(start_time = s,end_time = e)
-		# # print(lect1.start_time.hour,lect1.end_time.hour)
-		# # print(lect1)
 		periods = timings.objects.all().order_by('start_time')
-		# sorted_periods = periods
 		periods[0].delta()
 		events = event_class.objects.all()
 		days = ['Monday','Tuesday','Wednesday','Thursday','Friday']#,'Saturday','Sunday'
 		context = {
 			'days': days,
 			'table_width': len(days) * 240,
-			# 'periods' : ['9-9:50','9:50-10:40','10:40-11:30'],
 			'periods' : periods,
 			'events' : events,
 		}
@@ -59,3 +46,19 @@ class view_table(View):
 				return JsonResponse({"success": True}, status=200)
 		else:
 			return JsonResponse({"success": False}, status=400)
+
+
+########### adding objects
+# dbms = event_class(event_name = 'DBMS',event_link = "",event_color = "red")
+# ds = event_class(event_name = 'DS',event_link = "",event_color = "#0f0")
+# aem = event_class(event_name = 'AEM',event_link = "",event_color = "salmon")
+# math = event_class(event_name = 'Math',event_link = "",event_color = "cyan")
+# dbms.save()
+# ds.save()
+# aem.save()
+# math.save()
+# s = datetime.time(1,30,0)
+# e = datetime.time(2,30,0)
+# # lect1 = timings.objects.create(start_time = s,end_time = e)
+# # print(lect1.start_time.hour,lect1.end_time.hour)
+# # print(lect1)

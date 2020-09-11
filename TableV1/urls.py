5d9282bf5d51afd1cf8beb611f83e1710cd68921
Dev Patel <56962517+dmp060901@users.noreply.github.com>
2020-09-12 00:33:02 +0530

    Added comments to table views and changed abc.html to select_days.html

--- TableV1/urls.py
+++ TableV1/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('abc',view_table.selectday,name = 'day_form'),
+    path('select_days',view_table.selectday,name = 'day_form'),
     path('',view_table.as_view(),name = "table"),
     # path('cde',view_nav),
 ]
--- TableV1/views.py
+++ TableV1/views.py
@@ -1,34 +1,40 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.utils.decorators import method_decorator
-from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse, Http404
 from django.views import View
-from json import dumps
 #################
 import json
 import datetime
 from .models import event_class,timings,event
-from .forms import selectdays
+from .forms import selectdays	# for selecting the days
 ######################
 
 global_days = ['Monday','Tuesday','Wednesday','Thursday','Friday']
 class view_table(View):
-	template_name = "Table/table.html"
+	template_name = "Table/table.html"	# Main Table
 	global global_days
 	@method_decorator(ensure_csrf_cookie)
 	def get(self, request):
 		periods = timings.objects.filter(owner=request.user).order_by('start_time')
+		# Get all the timimg obojects of a user and sort them
+
 		event_butts = event_class.objects.filter(owner=request.user)
+		# Get all the event_buttons for a user 
+
 		events = event.objects.filter(owner=request.user)
+		#	Get all the exesting objects of the user
+
 		for e in events:
 			print(e.time_obj,e.day)
+
 		context = {
-			'days': global_days,
-			'day_len': len(global_days)+1,
-			'table_width': len(global_days) * 200,
-			'periods' : periods,
-			'event_butts' : event_butts,
-			'events' : events,
+			'days': global_days, 					# For displaying days
+			'table_width': len(global_days) * 200,	# For the width of the table
+			'periods' : periods,					# All the timings
+			'event_butts' : event_butts,			# All the buttons
+			'events' : events,						# All the existing events
+			# 'day_len': len(global_days)+1,		# For the tfoot button
 		}
 		return render(self.request, self.template_name,context)
 
@@ -36,53 +42,50 @@ class view_table(View):
 	def post(self, request):
 		if request.method == "POST" and request.is_ajax():
 			try:
-				data = json.loads(request.body)
-				print(data)
+				data = json.loads(request.body)	# data is the json object returned after saving
+				# print(data)
 				for i in data:
-					for day in global_days:
-						value = i[day]
+					for day in global_days:	# for all the days in the table
+						value = i[day]		# value is the event-object
 						if value['name'] and value['event_pk'] and value['time_pk']:
-							# print(i[day])
-							temp_obj = event_class.objects.filter(owner=request.user)
-							event_obj = temp_obj.get(pk=value['event_pk'])
-							temp_obj = timings.objects.filter(owner=request.user)
-							time_obj = temp_obj.get(pk=value['time_pk'])
-							# times = timings.objects.filter(user = request.user)
-							print(request.user.id)
-							obj = event(event_obj = event_obj,time_obj = time_obj,day=value['day'],owner=request.user)
-							print(obj)
-							obj.save()
+							try:
+								temp_obj = event_class.objects.filter(owner=request.user)
+								# temp_obj is the all the event_class objects of the user
+
+								event_obj = temp_obj.get(pk=value['event_pk']) or None
+								# event_obj is the same event_obj if it is of the same user
+								# else it is null
+
+								temp_obj = timings.objects.filter(owner=request.user)
+								# temp_obj is the all the timing objects of the user
+
+								time_obj = temp_obj.get(pk=value['time_pk'])
+								# time_obj is the same time_obj if it is of the same user
+								# else it is null
+
+								obj = event(event_obj = event_obj,time_obj = time_obj,day=value['day'],owner=request.user)
+								# made a event object succefully
+								obj.save()
+							except:
+								print("Stop Messing Around ...")
+								# if the primary keys of any of the objects do not match
+
 				return HttpResponse("<p>Done</p>")
+
 			except KeyError:
 				HttpResponseServerError("Malformed data!")
 				return JsonResponse({"success": True}, status=200)
+			return render(self.request, self.template_name,context)
 		else:
 			return JsonResponse({"success": False}, status=400)
 
 	def selectday(request):
+		# renders the form to select the days to display in the table
 		form = selectdays()
 		if request.method == 'POST':
 			form = selectdays(request.POST)
 			global global_days
 			global_days = request.POST.getlist('Days')
+			# changing the global days variable so when the get is called it has new days
 			return redirect('table')
-		return render(request,"abc.html",{'form':form})
-
-
-
-# def view_nav(request) :
-# 	return render(request,'navbar.html')
-########### adding objects
-# dbms = event_class(event_name = 'DBMS',event_link = "",event_color = "red")
-# ds = event_class(event_name = 'DS',event_link = "",event_color = "#0f0")
-# aem = event_class(event_name = 'AEM',event_link = "",event_color = "salmon")
-# math = event_class(event_name = 'Math',event_link = "",event_color = "cyan")
-# dbms.save()
-# ds.save()
-# aem.save()
-# math.save()
-# s = datetime.time(1,30,0)
-# e = datetime.time(2,30,0)
-# # lect1 = timings.objects.create(start_time = s,end_time = e)
-# # print(lect1.start_time.hour,lect1.end_time.hour)
-# # print(lect1)
+		return render(request,"Table/select_days.html",{'form':form})

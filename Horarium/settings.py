3497205833102800fc91991bc15cd70b0495ceef
Dev Patel <56962517+dmp060901@users.noreply.github.com>
2020-09-16 18:33:57 +0530

    Added mysql and it worked
    pip install mysqlclient

--- Horarium/settings.py
+++ Horarium/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Horarium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 

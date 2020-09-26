333fd2811bb9b985a075bec2065b2532eb3b1681
yogeshrathod2508 <yogeshrathod2508@users.noreply.github.com>
2020-09-26 16:58:46 +0530

    Added Toggler to switch between navbar to navtree
    Also changed home.css

--- static/navbar/navbar.js
+++ static/navbar/navbar.js
@@ -1,23 +1,36 @@
-$(document).ready(function() {
-    var scroll = $(document).scrollTop();
-    var navHeight = $('.navv').outerHeight();
+// $(document).ready(function() {
+//     var scroll = $(document).scrollTop();
+//     var navHeight = $('.navv').outerHeight();
+//
+//     $(window).scroll(function(){
+//
+//       var scrolled = $(document).scrollTop();
+//
+//       if(scrolled > navHeight){
+//         $('.navv').addClass('animate');
+//       }
+//       else {
+//         $('.navv').removeClass('animate');
+//       }
+//       if(scrolled > scroll){
+//         $('.navv').removeClass('sticky');
+//       }
+//       else {
+//         $('.navv').addClass('sticky');
+//       }
+//       scroll = (document).scrollTop();
+//     });
+//   });
 
-    $(window).scroll(function(){
+  function toggle() {
+    var x = document.getElementById("show_tree");
+    var y = document.getElementById("show_navbar");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "inline";
+    }
 
-      var scrolled = $(document).scrollTop();
-
-      if(scrolled > navHeight){
-        $('.navv').addClass('animate');
-      }
-      else {
-        $('.navv').removeClass('animate');
-      }
-      if(scrolled > scroll){
-        $('.navv').removeClass('sticky');
-      }
-      else {
-        $('.navv').addClass('sticky');
-      }
-      scroll = (document).scrollTop();
-    });
-  });
+  }

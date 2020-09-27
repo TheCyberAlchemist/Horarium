3d95d846ed951136064813f8d16ef5662d0c5b4b
yogeshrathod2508 <yogeshrathod2508@users.noreply.github.com>
2020-09-27 12:57:01 +0530

    Added Department Form with small changes in css files

--- static/admin/navbar/navbar.js
+++ static/admin/navbar/navbar.js
@@ -21,16 +21,17 @@
 //       scroll = (document).scrollTop();
 //     });
 //   });
-
   function toggle() {
     var x = document.getElementById("show_tree");
     var y = document.getElementById("show_navbar");
+    var z = document.getElementById("form_container_id");
     if (x.style.display === "none") {
       x.style.display = "block";
       y.style.display = "none";
+      z.style.display = "none";
     } else {
       x.style.display = "none";
       y.style.display = "inline";
+      z.style.display = "";
     }
-
   }

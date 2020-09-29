aad83c26efa15b1a9766df784ffc29dbc994ffa1
yogeshrathod2508 <yogeshrathod2508@users.noreply.github.com>
2020-09-29 16:16:31 +0530

    Action button, navtree toggle button, scss files

--- static/admin/navbar/navbar.js
+++ static/admin/navbar/navbar.js
@@ -1,30 +1,8 @@
-// $(document).ready(function() {
-//     var scroll = $(document).scrollTop();
-//     var navHeight = $('.navv').outerHeight();
-//
-//     $(window).scroll(function(){
-//
-//       var scrolled = $(document).scrollTop();
-//
-//       if(scrolled > navHeight){
-//         $('.navv').addClass('animate');
-//       }
-//       else {
-//         $('.navv').removeClass('animate');
-//       }
-//       if(scrolled > scroll){
-//         $('.navv').removeClass('sticky');
-//       }
-//       else {
-//         $('.navv').addClass('sticky');
-//       }
-//       scroll = (document).scrollTop();
-//     });
-//   });
-  function toggle() {
+
+function toggle() {
     var x = document.getElementById("show_tree");
     var y = document.getElementById("show_navbar");
-    var z = document.getElementById("form_container_id");
+    var z = document.getElementById("whole_container_id");
     if (x.style.display === "none") {
       x.style.display = "block";
       y.style.display = "none";

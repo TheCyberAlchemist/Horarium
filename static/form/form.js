9d9a7f8189e4eb86713f912796295c9e4a7cab26
yogeshrathod2508 <56973819+yogeshrathod2508@users.noreply.github.com>
2020-11-29 09:54:57 +0530

    Can teach Checkboxes
    Checkbox js in form.js
    CSS remaining

--- static/form/form.js
+++ static/form/form.js
@@ -28,6 +28,27 @@ $(document).ready(function(){
   });
 // /* //////Scroll into view ///////// */
 
+
+/**//////////// Can Teach ///////////////////
+  $('li :checkbox').on('click', function () {
+    var $chk = $(this);
+    var $li = $chk.closest('li');
+    var $ul, $parent;
+    if ($li.has('ul')) {
+        $li.find(':checkbox').not(this).prop('checked', this.checked);
+    }
+    do {
+        $ul = $li.parent();
+        $parent = $ul.siblings(':checkbox');
+        if ($chk.is(':checked')) {
+            $parent.prop('checked', $ul.has(':checkbox:not(:checked)').length == 0)
+        } else {
+            $parent.prop('checked', false)
+        }
+        $chk = $parent;
+        $li = $chk.closest('li');
+    } while ($ul.is(':not(.someclass)'));
+  });
 });
 
 function visibility1(self) {

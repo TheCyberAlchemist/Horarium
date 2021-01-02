8e61ba877fb5dec4866bf65fcc53c188c7cdba00
Dev Patel <56962517+dmp060901@users.noreply.github.com>
2021-01-02 11:30:25 +0530

    made a little progress in timetable.js
    the event listner of the submit button is not stopping

--- static/try/time_table.js
+++ static/try/time_table.js
@@ -1,25 +1,29 @@
+function print(abc){
+	console.log(abc);
+}
 function put_data(slots_json,events_json){
 	json = JSON.parse(slots_json.replace(/&#34;/ig,'"',));
 	for (i in json){
-		temp_slot = new slot(json[i].pk,json[i].fields.day,json[i].fields.Timing_id)
+		temp_slot = new slot(json[i].pk,json[i].fields.day,json[i].fields.Timing_id,json[i].fields.resources_filled)
 		slots.push(temp_slot);
 	}
+	// console.table(slots);
 	events_json = JSON.parse(events_json.replace(/&#34;/ig,'"',));
-	// console.log(events_json);
 	for (i in events_json){
 		obj = events_json[i].fields;
 		temp_event = new subject_event(events_json[i].pk,obj.Subject_id,obj.prac_carried,obj.lect_carried,obj.Faculty_id,obj.not_available,obj.other_events)
 		subject_events.push(temp_event);
 	}
-	console.table(subject_events);
+	// console.table(subject_events);
 }
 
 var slots = [];
 class slot{
-	constructor(id = 0,day = 0,timing = 0){
+	constructor(id = 0,day = 0,timing = 0,resources_filled=0){
 		this.id = id;
 		this.day = day;
 		this.timing = timing;
+		this.resources_filled = resources_filled;
 	}
 }
 
@@ -39,9 +43,10 @@ class subject_event {
 
 events = [];
 class event_class {
-	constructor(slot,subject_event,resource){
+	constructor(slot,subject_event,batch,resource){
 		this.slot = slot;
 		this.subject_event = subject_event;
+		this.batch = batch;
 		this.resource = resource;
 
 	}
@@ -51,12 +56,49 @@ class event_class {
 function get_slot(td){
 	let day = td[0].cellIndex;
 	let time = td.parent().attr("timing_id");
-	console.log(slots[0],day,time);
+	// console.log(slots[0],day,time);
 	for (i in slots){	
 		if (slots[i].day == day && slots[i].timing == time){
-			return slots[i].id;
+			return slots[i];
 		}
 	}
+}
+
+function get_subject_event(id){
+	// console.log(slots[0],day,time);
+	for (j in subject_events){
+		if (subject_events[j].id == id){
+			return subject_events[j]
+		}
+	}
+}
+
+function push_event(temp_event){
+	for(i in events){
+		if (events[i] == temp_event){
+			console.log("duplicate");
+			return;
+		}else if (events[i].slot == temp_event.slot){
+			// if there is en event on the slot
+			b1 = events[i].batch
+			b2 = temp_event.batch
+			if (b1 == b2){
+				// if lect-lect or prac(b1)-prac(b1)
+				console.log("lect-lect or prac(b1)-prac(b1)");
+				events.splice(i,1);
+			}else{
+				if (b1 && b2){
+					// if both not null prac(b1)-prac(b2)
+					console.log("prac(b1)-prac(b2)");
+				}else{
+					// if lect-prac or prac-lect
+					console.log("lect-prac or prac-lect");
+					events.splice(i,1);
+				}
+			}
+		}
+	}
+	events.push(temp_event);
 }
 
 function get_cell(slot_id){
@@ -94,41 +136,39 @@ $(document).ready (function () {
 		helper: "clone",
 		cursorAt:{top:56,left:56},
 		start: function(event, ui) {
-			subject_event_id = $(this).attr("subject_event_id");
 			// get all the td in which the faculty is not_available
-			for (j in subject_events){
-				if (subject_events[j].id == subject_event_id){
-					// for all the not_available td
-					for (i in subject_events[j].not_available){
-						td = get_cell(subject_events[j].not_available[i]);
-						if (!td.hasClass("filled")){
-							td.addClass("not_available_td");
-						}
-					}
-					// for all the other_events td
-					for (k in subject_events[j].other_events){
-						var obj = subject_events[j].other_events[k]['fields']
-						event_td = get_cell(obj.Slot_id);
-						if (!event_td.hasClass("filled")){
-							event_td.addClass("not_available_td");
-							event_td.html(obj.Division_id)
-						}
+			let subject_event = get_subject_event($(this).attr("subject_event_id"));
+				// for all the not_available td
+				for (i in subject_event.not_available){
+					td = get_cell(subject_event.not_available[i]);
+					if (!td.hasClass("filled")){
+						td.addClass("not_available_td");
 					}
 				}
-			}
+				// for all the other_events td
+				for (k in subject_event.other_events){
+					var obj = subject_event.other_events[k]['fields']
+					event_td = get_cell(obj.Slot_id);
+					if (!event_td.hasClass("filled")){
+						event_td.addClass("not_available_td");
+						// event_td.html(obj.Division_id)
+					}
+				}
 			if ($(this).attr("is_prac")){
 				$("td").each(function(){
 					var cellIndex = $(this).index();
 					var td_below = $(this).closest('tr').next().children().eq(cellIndex);
-					if (td_below.hasClass("not_available_td") || td_below.hasClass("filled") || td_below.hasClass("isBreak") || !td_below.length){
+					// console.log(td_below.hasClass("not_available_td"));
+					if ($(this).hasClass("not_available_td") || td_below.hasClass("not_available_td") || td_below.hasClass("filled") || td_below.hasClass("isBreak") || !td_below.length){
 						// if below is not available or filled or is break then not viable
-						$(this).addClass("not_available_td");
+						if (!$(this).hasClass("available_td") && !$(this).hasClass("isBreak"))
+							$(this).addClass("not_available_td");
 					}
-					else if (!td.hasClass("not_available_td")){	//	all the available
+					else{	//	all the available
+						// console.log("helr");
 						$(this).addClass("available_td");
-						// $(this).addClass("available_td");
-						// td_below.removeClass("not_available_td");
-						// td_below.addClass("available_td");
+						td_below.removeClass("not_available_td");
+						td_below.addClass("available_td");
 					}
 				});
 			}
@@ -153,16 +193,54 @@ $(document).ready (function () {
 		// on out
 		out: function( event, ui ) {
 		},
+		// on drop 
 		drop: function( event, ui ) {
-			let td = $( this );
-			if (!td.hasClass("not_available_td")){ // if faculty is available at this slot
-				let div = td.find("div");   // get child div of td
-				temp_event = new event_class(get_slot($(this)),subject_event_id);
-				events.push(temp_event);
-				console.table(temp_event);
-				td.addClass("filled");
-				td.css({"background-color":"red"})
+			if (!$( this ).hasClass("not_available_td")){ // if faculty is available at this slot
+				let td = $(this);
+				var slot = get_slot(td);
+				print(slot);
+				// all the options are enabled and then the filled resources are disabled 
+				$("#batches").next(".select2-container").show();
+				for (i in slot.resources_filled){
+					$("#resources option[value='"+String(slot.resources_filled[i])+"']").prop('disabled', 'disabled');
+				}
+				if (!ui.draggable.attr("is_prac")){
+					$("#batches").next(".select2-container").hide();
+				}
+				$("#event_form").show();
+				// $("#aform").one("click", function(){
+				$("#aform").on("click", function(){
+					let batch = $("#batches").val();
+					let resource = $("#resources").val();
+					print([batch,resource,slot.id]);
+					if (resource){
+						if (ui.draggable.attr("is_prac") && batch)
+							temp_event = new event_class(slot.id,ui.draggable.attr("subject_event_id"),batch,resource);
+						else if(!ui.draggable.attr("is_prac"))
+							temp_event = new event_class(slot.id,ui.draggable.attr("subject_event_id"),null,resource);
+						else
+							return;
+						push_event(temp_event);
+						console.table(events);
+						td.addClass("filled");
+						td.css({"background-color":"red"});
+					}
+					$("#event_form").hide();
+					return;
+				});
+
 			}
 		}
 	});
+	// lect1 = new event_class(2,7,null,1);
+	// lect2 = new event_class(2,3,null,1);
+	// prac1b1 = new event_class(2,7,1,1);
+	// prac2b1 = new event_class(2,3,1,1);
+	// prac2b2 = new event_class(2,7,2,1);
+	// // console.log(prac1b1.batch);
+	// push_event(lect1);
+	// push_event(new event_class(4,7,1,1));
+	// push_event(prac1b1);
+	// // push_event(new event_class(4,7,1,1));
+	// console.table(events);
 });

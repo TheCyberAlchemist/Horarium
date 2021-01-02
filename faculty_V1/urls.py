6be204b98b1f22b83a0480db60cc6834fed154c8
yogeshrathod2508 <yogeshrathod2508@users.noreply.github.com>
2021-01-02 20:19:23 +0530

    Added Faculty folder, added light-dark theme
    potato-potato - description same as title

---
+++ faculty_V1/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from django.conf.urls import url
+urlpatterns = [
+	path('home/',views.faculty_home,name = 'faculty_home'),
+]

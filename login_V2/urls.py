450e3e20a313019da7f1c5310509eef7bba717b8
yogeshrathod2508 <yogeshrathod19@gnu.ac.in>
2021-03-23 23:06:56 +0530

    About Page Started

--- login_V2/urls.py
+++ login_V2/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('',views.login_page,name ="login"),
     path('logout/',views.logout_user,name ="logout"),
     path('register/',views.register_page,name = "register"),
-    
+    path('about/',views.about,name = "about"),
+
     path('reset_password',auth_views.PasswordResetView.as_view(template_name="1.html"), name="reset_password"),
     path('reset_password_sent',auth_views.PasswordResetDoneView.as_view(template_name="login_V2/ForgotPassword/forgot_email_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="login_V2/ForgotPassword/forgot_password.html"), name="password_reset_confirm"),

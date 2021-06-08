b876aa2ab4387434aa672e49dc367a6cde550091
yogeshrathod2508 <yogeshrathod19@gnu.ac.in>
2021-06-08 18:55:02 +0530

    generate button modal ,wef progress bar
    Added modal for generate button,wef progress bar  function change

--- static/admin/details/wef_progress_bar.js
+++ static/admin/details/wef_progress_bar.js
@@ -9,8 +9,14 @@ $(document).ready(function() {
         //document.getElementsByClassName("progress-bar").style.backgroundColor = `rgb(${red},${green},3)`;
         return `rgb(${red},${green},3)`;
     };
+    function getColor(value){
+        //value from 0 to 1
+        value /= 100;
+        var hue=((1-value)*120).toString(10);
+        return ["hsl(",hue,",100%,50%)"].join("");
+    }
     $(".progress-bar").each(function(){
-        $(this).css("backgroundColor" , get_colour($(this).attr("aria-valuenow"))); 
+        $(this).css("backgroundColor" , getColor($(this).attr("aria-valuenow"))); 
         console.log(($(this).attr("aria-valuenow")));
         
     });

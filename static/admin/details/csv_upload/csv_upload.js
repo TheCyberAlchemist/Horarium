04aa437419fd2d7c6b03d47e0928dc1cd1ebcc41
yogeshrathod2508 <yogeshrathod19@gnu.ac.in>
2021-06-26 18:43:47 +0530

    Removed Dropzone and added another dropbox

--- static/admin/details/csv_upload/csv_upload.js
+++ static/admin/details/csv_upload/csv_upload.js
@@ -1,14 +1,11 @@
 if (!String.prototype.format) {
-	String.prototype.format = function() {
-		var args = arguments;
-		return this.replace(/{(\d+)}/g, function(match, number) { 
-		return typeof args[number] != 'undefined'
-			? args[number]
-			: match
-		;
-		});
-	};
-};
+  String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+      return typeof args[number] != "undefined" ? args[number] : match;
+    });
+  };
+}
 let s = `<div class="card shadow-sm components-section mb-4" style="border-radius: 1rem" id="error_card_{0}">
 	<div class="card-body" style="border-radius: 1rem;">
 		<div class="accordion accordion-flush" id="error_accordion_{0}">
@@ -35,59 +32,60 @@ let s = `<div class="card shadow-sm components-section mb-4" style="border-radiu
 		</div>
 	</div>
 </div>`;
-function make_accordian(index,error_heading,error_body,table){
-	let obj = $('<div/>').html(s.format(index,error_heading,error_body,table)).contents();
-	obj.find("thead").addClass("thead-light");
-	obj.find("th").addClass("fw-bold border-1");
-	return obj;
+function make_accordian(index, error_heading, error_body, table) {
+  let obj = $("<div/>").html(s.format(index, error_heading, error_body, table)).contents();
+  obj.find("thead").addClass("thead-light");
+  obj.find("th").addClass("fw-bold border-1");
+  return obj;
 }
 $(document).ready(function () {
-	console.log("asd");
-	var csrftoken = Cookies.get('csrftoken');
-	function csrfSafeMethod(method) {
-		// these HTTP methods do not require CSRF protection
-		return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-	}
-	// Ensure jQuery AJAX calls set the CSRF header to prevent security errors
-	$.ajaxSetup({
-		beforeSend: function(xhr, settings) {
-			if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-				xhr.setRequestHeader("X-CSRFToken", csrftoken);
-			}
-		}
-	});
+  console.log("asd");
 
-	// $.ajax({
-		// type: "post",
-		// success: function (data){
-			// 	// document.getElementsByClassName("response")[0].textContent = JSON.stringify(data, undefined, 2);
-			// }
-		// });
-		
-		$("#main_form").submit(function(e) {
-			// console.log(e);
-			e.preventDefault();
-			var formData = new FormData();
-			formData.append('file', $('#csv')[0].files[0]);			
-			$.ajax({
-				url:"../csv/",
-				type : 'POST',
-				data : formData,
-				processData: false,  // tell jQuery not to process the data
-				contentType: false,  // tell jQuery not to set contentType
-				success : function(data) {
-					if (data['error_list'].length > 0) {
-						show_errors(data['error_list']);
-					}
-				}
-			});
-		});
+  var csrftoken = Cookies.get("csrftoken");
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
+  }
+  // Ensure jQuery AJAX calls set the CSRF header to prevent security errors
+  $.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+      if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        xhr.setRequestHeader("X-CSRFToken", csrftoken);
+      }
+    },
+  });
+
+  // $.ajax({
+  // type: "post",
+  // success: function (data){
+  // 	// document.getElementsByClassName("response")[0].textContent = JSON.stringify(data, undefined, 2);
+  // }
+  // });
+
+  $("#main_form").submit(function (e) {
+    // console.log(e);
+    e.preventDefault();
+    var formData = new FormData();
+    formData.append("file", $("#csv")[0].files[0]);
+    $.ajax({
+      url: "../csv/",
+      type: "POST",
+      data: formData,
+      processData: false, // tell jQuery not to process the data
+      contentType: false, // tell jQuery not to set contentType
+      success: function (data) {
+        if (data["error_list"].length > 0) {
+          show_errors(data["error_list"]);
+        }
+      },
+    });
+  });
 });
-function show_errors(error_list){
-	// make the error_body as a li 
-	$("#error_main_container").html("");
-	for(i in error_list){
-		let obj = make_accordian(i,error_list[i].error_name,error_list[i].error_body,error_list[i].table);
-		$("#error_main_container").append(obj)
-	}
+function show_errors(error_list) {
+  // make the error_body as a li
+  $("#error_main_container").html("");
+  for (i in error_list) {
+    let obj = make_accordian(i, error_list[i].error_name, error_list[i].error_body, error_list[i].table);
+    $("#error_main_container").append(obj);
+  }
 }

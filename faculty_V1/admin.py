ee17dd6cf013f73eff21b6bb56c7d4ffa07e7ebf
yogeshrathod2508 <yogeshrathod19@gnu.ac.in>
2021-07-23 12:15:29 +0000

    server push before closing the instance

--- faculty_V1/admin.py
+++ faculty_V1/admin.py
@@ -8,5 +8,10 @@ admin.site.register(Faculty_load)
 admin.site.register(Can_teach)
 admin.site.register(Not_available)
 admin.site.register(Chart)
-admin.site.register(Feedback)
 admin.site.register(Feedback_type)
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+	list_display = ['Given_by','Faculty_id','average']
+	list_filter = ["timestamp"]
+	readonly_fields = ('timestamp',)
+# admin.site.register(Feedback)

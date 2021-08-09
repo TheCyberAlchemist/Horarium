a0c9cf84fb938ee63c4c8572eaacb14475d490fb
yogeshrathod2508 <yogeshrathod19@gnu.ac.in>
2021-08-09 15:07:22 +0530

    Removed USP list and added features list

--- static/landingpage/js/homepage_navbar.js
+++ static/landingpage/js/homepage_navbar.js
@@ -5,17 +5,11 @@ function scrollFunction() {
     document.getElementsByClassName("navbar-brand")[0].style.fontSize = "20px";
     document.getElementsByClassName("navbar-brand")[0].style.transition = ".5s";
     document.getElementsByClassName("navbar")[0].style.backgroundColor = "#066";
-<<<<<<< HEAD
 	document.getElementsByClassName("navbar")[0].style.transition = ".5s";
-=======
->>>>>>> 540346c22d2493fe8a56114680a1a2a6b4d1d0cc
 } else {
 	document.getElementsByClassName("navbar-brand")[0].style.fontSize = "22px";
     document.getElementsByClassName("navbar-brand")[0].style.transition = ".5s";
 	document.getElementsByClassName("navbar")[0].style.backgroundColor = "transparent";
-<<<<<<< HEAD
 	document.getElementsByClassName("navbar")[0].style.transition = ".5s";
-=======
->>>>>>> 540346c22d2493fe8a56114680a1a2a6b4d1d0cc
   }
 }

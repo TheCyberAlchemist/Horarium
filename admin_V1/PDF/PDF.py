105973c05dcfacac97bd0cae5add89276bf66233
Dev Patel <56962517+TheCyberAlchemist@users.noreply.github.com>
2021-08-27 23:35:09 +0530

    Removed excessive code from PDF.py utils.py and restective urls

--- admin_V1/PDF/PDF.py
+++ admin_V1/PDF/PDF.py
@@ -12,37 +12,6 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.generic import View
  
-#import render_to_pdf from util.py 
-from .utils import render_to_pdf 
-from pprint import pprint
-
-class GeneratePdf(View):
-	template = 'try/html2pdf/time_table_sample.html'
-	def get_events_by_batch(self,Division_id,prac_batch_list,lect_batch_list):
-		all_division_events = Event.objects.active().filter(Division_id=Division_id)
-		my_events = all_division_events.filter(Q(Batch_id=None) | Q(Batch_id__in = prac_batch_list) | Q(Batch_id__in =lect_batch_list))
-		print(my_events.count(),all_division_events.count())
-		
-	def post(self,request,*args,**kwargs):
-		Division_id = self.kwargs['Division_id']
-		print(all_division_events)
-		pdf = render_to_pdf(self.template,{"asd":"data","print":True})
-		# rendering the template
-		# pprint(list(all_division_events))
-
-		return HttpResponse(pdf, content_type='application/pdf')
-
-	def get(self, request, *args, **kwargs):
-		#getting the template
-		Division_id = self.kwargs['Division_id']
-
-		lect_batch_list=request.GET.getlist("lect_batch")
-		prac_batch_list=request.GET.getlist("prac_batch")
-		self.get_events_by_batch(Division_id,prac_batch_list,lect_batch_list)
-		
-		return render(request,self.template,{"asd":"data"})
-		# return HttpResponse(pdf, content_type='application/pdf')
-
 from admin_V1.views import return_context
 def select_batch_for_pdf(request,Division_id):
 	context = return_context(request)
@@ -60,8 +29,8 @@ def select_batch_for_pdf(request,Division_id):
 
 	return render(request,"admin/create_table/select_batch.html",context)
 
-def table_template(request,Division_id):
-	template = "admin/print_table/table_template.html"
+def division_print(request,Division_id):
+	template = "admin/print_table/division_print.html"
 	Division_id = Division.objects.get(pk=Division_id)
 	my_shift = Division_id.Shift_id
 
@@ -102,14 +71,6 @@ def table_template(request,Division_id):
 
 
 #endregion
-
-#region //////////////////// weasy_print //////////////////
-from django.template.loader import render_to_string
-# from weasyprint import HTML
-import pdfkit
-import tempfile
-from os import path,remove
-from django.conf import settings
 def lcm(x, y):
 	# choose the greater number
 	x = 1 if not x else x
@@ -121,31 +82,3 @@ def lcm(x, y):
 			break
 		greater += 1
 	return ans
-
-
-def export_pdf(template_name,file_name,context):
-	path_to_admin_pdf = path.join(settings.BASE_DIR,"admin_V1","PDF")
-	config = pdfkit.configuration(wkhtmltopdf=path.join(path_to_admin_pdf,"wkhtmltopdf","bin","wkhtmltopdf.exe"))
-	html_string = render_to_string(template_name,context)
-	
-	temp_file_path = path.join(path_to_admin_pdf,'temp.pdf')
-	css = [path.join(settings.STATIC_ROOT,"Bootstrap","bootstrap.min.css")]
-	pdf_binary_object = pdfkit.from_string(html_string,False,configuration=config,css=css)
-
-	response = HttpResponse(pdf_binary_object,content_type='application/pdf')
-	response['Content-Disposition'] = f'attachment; filename={file_name}.pdf'
-	response['Content-Transfer-Encoding'] = "binary"
-	
-	# with tempfile.NamedTemporaryFile(delete=True) as output:
-	# 	output.write(a)
-	# 	output.flush()
-	# 	o = open(output.name,'rb')
-	# 	response.write(o.read())
-	# remove(temp_file_path)
-	return response
-
-#endregion
-# print(export_pdf())
-# in template we could keep the download buttton in {% if print %}
-# this could be donw so that when we initially load the page as preview we keem print = faculty_settings
-# when we are converting the page to pdf we send print = true hence only the table will be printed
--- admin_V1/urls.py
+++ admin_V1/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
 
 	re_path(r'^resource/$',views.show_resource,name = 'show_resource'),
 	re_path(r'^resource/(?P<Resource_id>\d+)/$',views.show_resource,name = 'update_resource'),
+	re_path(r"get_shift_resources/\Z",views.get_unattached_resources_for_shift,name = 'get_unattached_resource'),
 
 	re_path(r'^branch/(?P<Department_id>\d+)/$',views.show_branch,name = 'show_branch'),
 	re_path(r'^branch/(?P<Department_id>\d+)/(?P<Branch_id>\d+)/$',views.show_branch,name = 'update_branch'),
@@ -52,7 +53,7 @@ urlpatterns = [
 	
 	re_path(r'^division/(?P<Semester_id>\d+)/$',views.show_division,name = 'show_division'),
 	re_path(r'^division/(?P<Semester_id>\d+)/(?P<Division_id>\d+)/$',views.show_division,name = 'update_division'),
-	
+
 	re_path(r'^batch/(?P<Division_id>\d+)/$',views.show_batch,name = 'show_batch'),
 	re_path(r'^batch/(?P<Division_id>\d+)/(?P<Batch_id>\d+)/$',views.show_batch,name = 'update_batch'),
 	
@@ -81,18 +82,11 @@ urlpatterns = [
 
 	re_path(r'^try/algo3/$',algo3.view_func,name = 'view_algo3'),
 
-	path("table_template",PDF.table_template,name="table_template"),
-    re_path(r'^print_table/$',PDF.export_pdf,name = 'print_table'),
-    re_path(r'^print4/$',views.print4,name = 'print4'),
-	re_path(r'^print_table2/$',views.GeneratePdf.as_view(),name = 'print_table2'),
-
 	re_path(r'^select_batches/(?P<Division_id>\d+)/$',PDF.select_batch_for_pdf,name = 'select_batch'),
-	re_path(r'^select_batches/(?P<Division_id>\d+)/print_table/$',PDF.table_template,name = 'print_table3'),
+	re_path(r'^select_batches/(?P<Division_id>\d+)/print_table/$',PDF.division_print,name = 'print_table3'),
 
     re_path(r'^text_editor/',views.text_editor,name = 'text_editor'),
 
 	# re_path("/algo",views.algo_v1,name = "algo"),
-	# path('sub/',views.show_sub_det,name = 'show_sub_det'),
-	# path('sube/',views.show_subject_events, name = 'show_subject_events')
 	# path('500/',views.error_500_view,name = '500'),
 ]

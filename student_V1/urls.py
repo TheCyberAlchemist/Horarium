a1dac27b716de7e263c19925e8e20c789303069c
yogeshrathod2508 <yogeshrathod19@gnu.ac.in>
2021-09-28 19:17:53 +0530

    Exam page for student panel added

--- student_V1/urls.py
+++ student_V1/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('gg/',views.sendMail,name = 'gg'),
 	re_path('home/get_mandatory_subjects/',views.get_all_subjects_of_feedback_type,name="mandatory_subjects"),
 	re_path('home/fill_mandatory_feedback',views.fill_mandatory_feedback,name="fill_mandatory_feedback"),
+	path('exam/',views.exam_timetable,name = 'exam_timetable'),
 ]
